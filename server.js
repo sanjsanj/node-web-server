91a809a9680f86f28a3e843284611cb22d996e03
Sanjay Purswani <sanjsanj@hotmail.com>
2018-09-08 14:13:18 +0100

    server ready for heroku deployment

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.path}`;
+  fs.appendFileSync("server.log", `${log}\n`)
+  console.log(log);
+  next();
+})
+
+app.use((req, res, next) => {
+  res.render("maintenance.hbs")
+})
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => new Date().getFullYear());
+hbs.registerHelper("screamIt", text => text.toUpperCase());
+
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    welcomeMessage: "Welcome to the site",
+    pageTitle: "Home Page"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "Error"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
