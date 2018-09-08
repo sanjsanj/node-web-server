569474323c17a93486f24a93db98c3978c632fb6
Sanjay Purswani <sanjsanj@hotmail.com>
2018-09-08 14:35:32 +0100

    add a projects page

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get("/bad", (req, res) => {
   });
 });
 
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Projects Page"
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
 });
